001708149ab95318760c7ea9746127a93f4d6178
David Härer <haerer@locandis.de>
2021-03-06 17:45:58 +0100

    Fix typo.

--- docs/script.js
+++ docs/script.js
@@ -2,6 +2,6 @@ window.onload = function() {
     fetch ("footer.html")
         .then(response => response.text())
         .then(footer => {
-            document.getElementByTagName("footer")[0].innerHTML = footer
+            document.getElementsByTagName("footer")[0].innerHTML = footer
         });
 }
