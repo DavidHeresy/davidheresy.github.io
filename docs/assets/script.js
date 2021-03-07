5cb10e98aeb04c615d0b4e409a8991de31ebd5df
David Härer <haerer@locandis.de>
2021-03-07 14:05:32 +0100

    Update.

---
+++ docs/assets/script.js
@@ -0,0 +1,40 @@
+window.onload = function() {
+    setHead();
+    setTitle();
+    setHeader();
+    setFooter();
+}
+
+
+function setHead() {
+    fetch ("https://davidheresy.de/head.html")
+        .then(response => response.text())
+        .then(head => {
+            document.getElementsByTagName("head")[0].innerHTML += head;
+        });
+}
+
+
+function setTitle() {
+    let h1 = document.getElementsByTagName("h1")[0];
+    document.title = h1.innerHTML + " • davidheresy.de";
+}
+
+
+function setHeader() {
+    fetch ("https://davidheresy.de/header.html")
+        .then(response => response.text())
+        .then(header => {
+            document.getElementsByTagName("header")[0].innerHTML = header;
+        });
+}
+
+
+function setFooter() {
+    fetch ("https://davidheresy.de/footer.html")
+        .then(response => response.text())
+        .then(footer => {
+            document.getElementsByTagName("footer")[0].innerHTML = footer;
+        });
+}
+
